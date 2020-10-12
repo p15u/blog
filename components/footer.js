765f18792bea3c5cabd070c6763d97f4c2a4d984
p15u's <62756028+p15u@users.noreply.github.com>
2020-10-13 00:12:50 +0700

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { blog } from '../config.json'
 const Footer = () => (
     <footer>
         <p>
-            &copy; {new Date().getFullYear()} {blog.author}
+            &copy; {new Date().getFullYear()} {blog.author}'s
         </p>
         <p>
             <a href="https://ko-fi.com/nomisan" target="_blank" rel="noreferrer">
